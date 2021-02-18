d717b56e225f793b4574b8d454c82a5450aa60ff
Kole Morgan <kmorgan0057@gmail.com>
2021-02-18 16:06:55 -0600

    Narrative exploration cycles expanded

--- src/Gameplay/Hometown.java
+++ src/Gameplay/Hometown.java
@@ -96,6 +96,141 @@ public class Hometown {
     }
 
     public void visitLab(Trainer player, Trainer rival, Computer computer){
+        boolean southernLoop = true;
+        boolean northernLoop = true;
+        boolean inspectLoop = true;
+        System.out.println("You approach PROF LIVEOAK's lab. You look across the road to see a large pasture of POKEMON. A TAUROS feeds silently while a flock of PIDGEY flies by.");
+        System.out.println("You approach the large glass doors of the laboratory, do you go inside? Y/N");
+        //if yes
+        System.out.println("You enter the lab, a giant metal and glass structure with large floor to ceiling windows that look out into the sprawling fields below.");
+        System.out.println("You look around and see several research associates analyzing data on large monitors. Who do you want to talk to?");
+        while(southernLoop){
+            String rivalName = rival.getName();
+            System.out.println("--ScientistA--ScientistB--ScientistC--");
+            String answ = io.getString();
+            switch(answ.toUpperCase()){
+                case "SCIENTISTA":
+                    System.out.println("I study POKEMON endemic to KANTO in PROF LIVEOAK's lab!");
+                    System.out.println("Did you know that KANTO once had almost 300 kinds of POKEMON native to this region?");
+                    System.out.println(" ");
+                    System.out.println("Do you want to talk to anyone else? Y/N");
+                    answ = io.getString();
+                    if(answ.toLowerCase().equals("yes")||answ.toLowerCase().equals("y")){
+                        break;
+                    }else{
+                        southernLoop = false;
+                        break;
+                    }
+                case "SCIENTISTB":
+                    System.out.println("I help PROF LIVEOAK study the KANTO POKEMON repopulation program!");
+                    System.out.println("Did you know that many species that used to live in KANTO are still alive and thriving in other parts of the world?");
+                    System.out.println(" ");
+                    System.out.println("Do you want to talk to anyone else? Y/N");
+                    answ = io.getString();
+                    if(answ.toLowerCase().equals("yes")||answ.toLowerCase().equals("y")){
+                        break;
+                    }else{
+                        southernLoop = false;
+                        break;
+                    }
+                case "SCIENTISTC":
+                    System.out.println("PROF LIVEOAK is the authority on POKEMON migrations! His colleagues across the world hold him in high regard!");
+                    System.out.println("Did you know PROF LIVEOAK traveled the world to study where KANTO's POKEMON originated?");
+                    System.out.println(" ");
+                    System.out.println("Do you want to talk to anyone else? Y/N");
+                    answ = io.getString();
+                    if(answ.toLowerCase().equals("yes")||answ.toLowerCase().equals("y")){
+                        break;
+                    }else{
+                        southernLoop = false;
+                        break;
+                    }
+                default:
+                    System.out.println("Sorry, there might be a typo.");
 
+            }
+        }
+        System.out.println("You walk north further into the lab. You see a table with three POKEBALLS resting on top. " + rival.getName() + " is standing flipping thru his phone, it sounds like he is watching a match.");
+        System.out.println(" ");
+        while(northernLoop){
+            System.out.println("What do you do?");
+            System.out.println("--INSPECT--RIVAL");
+            String answ = io.getString();
+            if(answ.toUpperCase().equals("INSPECT")){
+                System.out.println(" ");
+                while(inspectLoop){
+                    System.out.println("What would you like to inspect?");
+                    System.out.println("--TABLE--COMPUTER--DESK--DISPLAY");
+                    answ = io.getString();
+                    switch(answ.toUpperCase()){
+                        case "TABLE":
+                            System.out.println("You approach the three POKEBALLS closer. You haven't actually ever held one before. Are they...vibrating?");
+                            System.out.println("You reach out to touch one...actually....on second thought, maybe not");
+                            System.out.println(" ");
+                            System.out.println("Do you want to inspect anything else? Y/N");
+                            answ = io.getString();
+                            if(answ.toLowerCase().equals("yes")||answ.toLowerCase().equals("y")){
+                                break;
+                            }else{
+                                inspectLoop = false;
+                                break;
+                            }
+                        case "COMPUTER":
+                            System.out.println("You approach the computer, the screen is locked");
+                            System.out.println("You see a picture of PROF LIVEOAK with DAISY and " + rival.getName() + " MAGIKARP fishing.");
+                            System.out.println("...");
+                            System.out.println("There's another picture on his desk");
+                            System.out.println("Huh, it's a picture of him with you and your mom from when you were very little. Your snowsuit makes you look like a little TEDIURSA and you're building snow SNORUNTS");
+                            System.out.println("That's so weird.....you've never seen this picture before'....");
+                            System.out.println(" ");
+                            System.out.println("Do you want to inspect anything else? Y/N");
+                            answ = io.getString();
+                            if(answ.toLowerCase().equals("yes")||answ.toLowerCase().equals("y")){
+                                break;
+                            }else{
+                                inspectLoop = false;
+                                break;
+                            }
+                        case "DESK":
+                            System.out.println("You approach the desk, you see what looks like a red tablet lying on top");
+                            System.out.println("It looks expensive, best not to touch it");
+                            System.out.println(" ");
+                            System.out.println("Do you want to inspect anything else? Y/N");
+                            answ = io.getString();
+                            if(answ.toLowerCase().equals("yes")||answ.toLowerCase().equals("y")){
+                                break;
+                            }else{
+                                inspectLoop = false;
+                                break;
+                            }
+                        case "DISPLAY":
+                            System.out.println("You see a glass display that looks like it's hooked up to expensive equipment, what's inside....");
+                            System.out.println("...");
+                            System.out.println("It's just....a single piece of pinkish-white hair? What's so special about that?");
+                            System.out.println(" ");
+                            System.out.println("Do you want to inspect anything else? Y/N");
+                            answ = io.getString();
+                            if(answ.toLowerCase().equals("yes")||answ.toLowerCase().equals("y")){
+                                break;
+                            }else{
+                                inspectLoop = false;
+                                break;
+                            }
+                        default:
+                            System.out.println("Sorry, there might be a typo.");
+                    }
+                }
+            }else if(answ.toUpperCase().equals("RIVAL")){
+                System.out.println("Oh....it's you...");
+                System.out.println("Gramps isn't here, he's out looking for you.");
+                System.out.println("Well I dunno where he is, leave me alone, this match is good. This GENGAR is going to destroooyyy this JIGGLYPUFF.\n What kind of a moron would send out a FAIRY type against a POISON type??");
+                System.out.println("");
+                System.out.println("He looks busy, you should probably get going to find PROF LIVEOAK");
+                System.out.println(" ");
+                System.out.println("You wave goodbye to the research associates");
+                northernLoop = false;
+            }
+        }
+        //ifno
     }
 }
